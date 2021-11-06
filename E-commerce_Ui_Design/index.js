d9bcd4ac17ef98cd18093347cedce6bb8343aeab
tarun <arunaggarwal096@gmail.com>
2021-11-06 09:23:44 +0530

    e-commerce

---
+++ E-commerce_Ui_Design/index.js
@@ -0,0 +1,19 @@
+let menuItem = document.getElementById("menuItems");
+menuItem.style.maxHeight = "0px";
+
+function menuToggle(){
+    if(menuItem.style.maxHeight == "0px"){
+        menuItem.style.maxHeight = "200px";
+    }else{
+        menuItem.style.maxHeight = "0px";
+    }
+}
+
+
+
+
+
+
+
+
+
