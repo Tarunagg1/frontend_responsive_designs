0d972f9c67b18b8b6c07a4798e4fdd52a40258cb
tarun <arunaggarwal096@gmail.com>
2021-12-25 12:28:11 +0530

    portfolio_design final

--- portfolio_design/index.js
+++ portfolio_design/index.js
@@ -1,1 +1,21 @@
-console.log("i'm javascript");
+
+const hamburger = document.querySelector(".header .nav-bar .nav-list .hamburger");
+const mobile_menu = document.querySelector(".header .nav-bar .nav-list ul");
+const header = document.querySelector(".header.container");
+
+
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    mobile_menu.classList.toggle("active");
+})
+
+
+document.addEventListener("scroll", () => {
+    let scroll_Position = window.scrollY;
+    if (scroll_Position > 250) {
+        header.style.backgroundColor = "#29323c";
+    } else {
+        header.style.backgroundColor = "transparent";
+    }
+})
+
