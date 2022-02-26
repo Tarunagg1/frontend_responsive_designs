7b4cf276cdcf0021c7ccecc0f28979abb2609b23
tarun <tarun@iotric.com>
2022-02-26 10:56:37 +0530

    foodorder part1

---
+++ food_order/index.js
@@ -0,0 +1,27 @@
+AOS.init();
+const navIcon = document.querySelector('.nav__icon');
+const navClose = document.querySelector('.nav__close');
+const nav_List = document.querySelector('.nav__list');
+const nav__bgOverlay = document.querySelector('.nav__bgOverlay');
+
+const navOpen = () => {
+    nav_List.classList.add('show');
+    nav__bgOverlay.classList.add('active');
+    document.body.style = 'visibility: visible; height: 100vh; width:100vw; overflow: hidden;'
+}
+
+const closeOpen = () => {
+    nav_List.classList.remove('show');
+    nav__bgOverlay.classList.remove('active');
+    document.body.style = 'visibility: visible; height: initial; width:100vw; overflow-x: hidden;'
+}
+
+navIcon.addEventListener('click', navOpen);
+nav__bgOverlay.addEventListener('click', closeOpen);
+navClose.addEventListener('click', closeOpen);
+
+
+
+
+
+
